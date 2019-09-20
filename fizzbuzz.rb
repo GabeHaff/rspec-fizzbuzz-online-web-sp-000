9fe896348f6e99ba4aa2e624a4aa98c254cd94c1
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-09-20 12:21:41 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,5 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(integer)
-  if integer % 3 == 0 
-    puts "Fizz"
-    elsif integer % 5==0 
-    puts "Buzz"
-    elsif integer % 3 && % 5
-    puts "FizzBuzz"
-  else puts "nil"
+ 
 end
